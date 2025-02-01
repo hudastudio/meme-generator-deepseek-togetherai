0f78e10dc02f634c5e70928d8b548c68a2b6241d
Mochammad Miftahul Huda Firmansyah <iniakunhuda@gmail.com>
2025-02-01 14:23:56 +0700

    feat(main): add button github bottom of page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import MemeGenerator from '@/components/MemeGenerator'
+import { Button } from '@/components/ui/button';
+import { Github } from 'lucide-react';
 
 function App() {
   return (
@@ -9,6 +11,16 @@ function App() {
           DeepSeek AI Gratis dari <a href="https://api.together.ai/">https://api.together.ai/</a>
         </p>
         <MemeGenerator />
+        <div className="text-center mt-10">
+          <a
+              className="text-center text-sm text-slate-500 hover:text-slate-700"
+              style={{ cursor: 'pointer' }}
+              onClick={() => window.open('https://github.com/hudastudio/meme-generator-deepseek-togetherai/', '_blank')}
+          >
+              <Github size={16} className="inline-block mr-2" />
+              Github @ hudastudio
+          </a>
+        </div>
       </div>
     </div>
   )
